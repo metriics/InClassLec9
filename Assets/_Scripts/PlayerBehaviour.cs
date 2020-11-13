181beb320a4ab39ca3c38706be57bd4aff42954e
Steph <stephane.gagnon00@gmail.com>
2020-11-13 17:36:29 -0500

    huh

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -6,9 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
     public bool isGrounded;
+    public float horizontalForce;
+    public float verticalForce;
+
     private Rigidbody2D m_rigidBody2D;
     private SpriteRenderer m_spriteRenderer;
     private Animator m_animator;
+
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
@@ -19,19 +24,63 @@ public class PlayerBehaviour : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isGrounded)
+        {
+            m_rigidBody2D.AddForce(direction * horizontalForce * Time.fixedDeltaTime);
+
+            m_rigidBody2D.velocity *= 0.95f;
+        }
+        if (Mathf.Abs(m_rigidBody2D.velocity.x) > 0.1f)
+        {
+            m_animator.SetInteger("AnimState", 1);
+        }
+        else
+        {
+            m_animator.SetInteger("AnimState", 0);
+
+        }
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        Debug.Log("Move Activated");
+        direction = context.ReadValue<Vector2>();
+
+        switch (context.control.name)
+        {
+            case "a":
+            case "leftArrow":
+                m_spriteRenderer.flipX = true;
+                m_animator.SetInteger("AnimState", 1);
+                break;
+
+            case "d":
+            case "rightArrow":
+                m_spriteRenderer.flipX = false;
+                m_animator.SetInteger("AnimState", 1);
+                break;
+
+        }
+        
+    }
+
+    private void _MoveLeft()
+    {
+        //m_rigidBody2D.AddForce(Vector2.left * horizontalForce);
+
+    }
+
+    private void _MoveRight()
+    {
+        //m_rigidBody2D.AddForce(Vector2.right * horizontalForce);
+
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        Debug.Log("Jump Activated");
+        m_rigidBody2D.AddForce(Vector2.up * verticalForce);
     }
 
     public void OnCollisionEnter2D(Collision2D other)
